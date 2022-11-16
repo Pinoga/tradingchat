47de002243e1562f4b36529e868a9f87edad0e4f
fernando-bizzotto <Fernando.Bizzotto@mheducation.com>
2022-11-16 09:44:47 -0300

    Docker tag and push

--- deploy/build-image.sh
+++ deploy/build-image.sh
@@ -2,24 +2,25 @@
 
 REGION=$1
 ACCOUNT=$2
-REPOSITORY=$3
+SCOPE=$3
 PROJECT=$4
 HASH=$5
 BUILD=$6
 
-REPO_URL="$ACCOUNT.dkr.ecr.$REGION.amazonaws.com/$REPOSITORY"
+REGISTRY="$ACCOUNT.dkr.ecr.$REGION.amazonaws.com"
+REPOSITORY_URL="$REGISTRY"/"$SCOPE"/"$PROJECT"
 
 aws ecr get-login-password --region "$REGION" \
 | docker login \
   -u AWS \
   --password-stdin \
-  "$REPO_URL"
+  "$REPOSITORY_URL"
 
-TAG="$PROJECT":"$HASH"-"$BUILD"
+TAG="$HASH"-"$BUILD"
 
 ls -las
 
-docker build -t "$REPO_URL"/"$TAG" -f deploy/"$PROJECT"/Dockerfile .
+docker build -t "$REPOSITORY_URL"/"$TAG" -f deploy/"$PROJECT"/Dockerfile .
 
-docker push "$REPO_URL"/"$TAG"
+docker push "$REPOSITORY_URL"/"$TAG"
 
